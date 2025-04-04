0efe3d6dfebedf5047d06e540aa4401583f58de4
NongTonnamLuvU <nong.touuam@gmail.com>
2025-04-04 08:58:24 +0700

    Update A2-013.py

--- A2-013.py
+++ A2-013.py
@@ -1,29 +1,42 @@
-x = str(input())
-y = str(input())
+x = input().split()
+y = input().split()
 
-km = x[2:]
+if len(x) < 2 or len(y) < 3:
+    exit(1)
+
+if not x[1].isdigit() or not y[1].isdigit() or not y[2].isdigit():
+    exit(1)
+
+km = int(x[1])
+
 if x[0] == "H":
-    ck = 5*int(km)
+    ck = 5 * km
 elif x[0] == "O":
-    ck = 3*int(km)
+    ck = 3 * km
 elif x[0] == "J":
-    ck = 2*int(km)
-
-R = [ 12, 18, 25]
-T = [ 15, 20, 30]
-M = [ 10, 15, 20]
-
-cha = y[4:]
-if y[0] == "R" and int(y[2]) > 0 and int(y[2]) < 3:
-    c = R[int(y[2])-1] * int(cha)
-elif y[0] == "T" and int(y[2]) > 0 and int(y[2]) < 3:
-    c = T[int(y[2])-1] * int(cha)
-elif y[0] == "M" and int(y[2]) > 0 and int(y[2]) < 3:
-    c = M[int(y[2])-1] * int(cha)
-
-if type(ck) == int and type(c) == int:
-    print(ck+c)
+    ck = 2 * km
 else:
-    exit()
+    exit(1)
+
+R = [12, 18, 25]
+T = [15, 20, 30]
+M = [10, 15, 20]
+
+v = y[0]  
+z = int(y[1])  
+cha = int(y[2])  
 
 
+if v == "R" and 1 <= z <= 3:
+    c = R[z - 1] * cha
+elif v == "T" and 1 <= z <= 3:
+    c = T[z - 1] * cha
+elif v == "M" and 1 <= z <= 3:
+    c = M[z - 1] * cha
+else:
+    exit(1)
+
+if isinstance(ck, int) and isinstance(c, int):
+    print(ck + c)
+else:
+    exit(1)
