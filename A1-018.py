31fc7d06467d632245aa3cc59bb0eb43a13818f1
NongTonnamLuvU <nong.touuam@gmail.com>
2025-05-01 19:50:43 +0700

    Update A1-018.py

--- A1-018.py
+++ A1-018.py
@@ -1,9 +1,12 @@
 x = int(input())
 f = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
 
-if x < 1:
+if x < 0:
     print("Error : Please input positive number")
-elif x == 0 or x > 10:
+elif x == 0 or x > 9:
     print("Error : Out of range")
 else:
     print(f[x-1])
+    
+
+    
